26d3850922380f1599d48d0e15039e83cd8562de
pookiedog11 <149290576+psychorax1433@users.noreply.github.com>
2025-04-06 19:26:15 +0530

    Add files via upload

---
+++ pg2(self).py
@@ -0,0 +1,7 @@
+def add(P, Q):
+    return P + Q
+print("Whould you like you addition problems quickly with a maths pro??")
+print( " i think a here yes!!")
+num_1 = int(input("Please enter the first number: "))
+num_2 = int(input("Please enter the second number: "))
+print(num_1, "+", num_2, "=", add(num_1, num_2))
